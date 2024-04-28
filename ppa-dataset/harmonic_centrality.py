37589bb8ca9445f78b1801209eeb3354d1fd73a2
PavlosPo <pavlos.poulos@outlook.com>
2024-04-28 11:27:20 +0300

    Added a lot of results, trying to finish harmonic and clossness centrality in the server

---
+++ ppa-dataset/harmonic_centrality.py
@@ -0,0 +1,33 @@
+import networkx as nx
+import pandas as pd
+from ogb.linkproppred import LinkPropPredDataset
+
+print('Loading dataset...')
+dataset = LinkPropPredDataset(name = 'ogbl-ppa')
+
+split_edge = dataset.get_edge_split()
+train_edge, valid_edge, test_edge = split_edge["train"], split_edge["valid"], split_edge["test"]
+graph = dataset[0] # graph: library-agnostic graph object
+
+G = nx.Graph()
+
+print('Creating graph...')
+for i in range(graph['num_nodes']):
+    source = graph['edge_index'][0][i]
+    target = graph['edge_index'][1][i]
+    G.add_edge(source, target)
+
+
+print('Calculating harmonic centrality...')
+# calculate harmonic centrality
+results = nx.harmonic_centrality(G)
+
+print('Saving results...')
+# Save in pd.DataFrame
+results_df = pd.DataFrame(results.items(), columns=['Node', 'Harmonic Centrality'])
+
+
+# Save results in csv
+results_df.to_csv('harmonic_centrality.csv', index=False)
+
+print('Done!')
